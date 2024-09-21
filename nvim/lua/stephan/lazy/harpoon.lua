4c99b29a0e3b3d38626aede61e4f7da9609360e8
Stephan Hobler <stephanhobler1@gmail.com>
2024-09-21 20:26:20 +0100

    Add harpoon

---
+++ nvim/lua/stephan/lazy/harpoon.lua
@@ -0,0 +1,44 @@
+return {
+  {
+    'theprimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('harpoon'):setup()
+
+      local harpoon = require 'harpoon'
+
+      -- REQUIRED
+      harpoon:setup()
+      -- REQUIRED
+
+      vim.keymap.set('n', '<leader>ha', function()
+        harpoon:list():add()
+      end)
+      vim.keymap.set('n', '<C-h>', function()
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end)
+
+      vim.keymap.set('n', '<leader>hh', function()
+        harpoon:list():select(1)
+      end)
+      vim.keymap.set('n', '<leader>hj', function()
+        harpoon:list():select(2)
+      end)
+      vim.keymap.set('n', '<leader>hk', function()
+        harpoon:list():select(3)
+      end)
+      vim.keymap.set('n', '<leader>hl', function()
+        harpoon:list():select(4)
+      end)
+
+      -- Toggle previous & next buffers stored within Harpoon list
+      vim.keymap.set('n', '<leader>hp', function()
+        harpoon:list():prev()
+      end)
+      vim.keymap.set('n', '<leader>hn', function()
+        harpoon:list():next()
+      end)
+    end,
+  },
+}
